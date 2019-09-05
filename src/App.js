803976206d04007a125fed4819e01aa671d1595a
Bradley Erickson <bbwe24@gmail.com>
2019-09-05 09:32:09 -0500

    Testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button>
+          doop
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
